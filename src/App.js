8cfdf0f93ea1c15271682bca47f58b5d6ad9bb35
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-27 22:35:27 +0600

    corected some news

--- src/App.js
+++ src/App.js
@@ -1,63 +1,145 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [firstName, setFirstName] = useState("first Name")
-  const [lastName, setLastName] = useState("last Name")
-  const [date, setDate] = useState()
-  const [lenguages, setLenguages] = useState()
-  const [jender, setJender] = useState()
-  const [address, setAddress] = useState()
-  const [city, setCity] = useState()
-  const [email, setEmail] = useState()
-  const [tel, setTel] = useState()
-  const [education, setEducation] = useState()
-  const [zip, setZip] = useState()
+  function formSubmitCallback(event) {
+    const data = new FormData(event.target);
+    const user = {
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'), 
+      gender: data.get('gender'),
+      firstName: data.get('email'),
+      firstName: data.get('phone'),
+      // ...
+    }
+    
+    console.log(user);
+    event.preventDefault();
+  }
 
   return (
-    <div className="App">
+    <form className="App" onSubmit={formSubmitCallback}>
       <div>
-        <input type="text" onChange={({ target }) => setFirstName(target.value)} /> first Name
+        <label for="firstName">First name</label>
+        <input type="text"
+          name="firstName"
+          id="firstName"
+          required />
       </div>
       <div>
-        <input type="text" onChange={({ target }) => setLastName(target.value)} /> last Name
+        <label for="lastName">Last name</label>
+        <input type="text"
+          name="lastName"
+          id="lastName"
+          required />
       </div>
       <div>
-        <input type="date" onChange={({ target }) => setDate(target.value)} />
+        <label for="email">Email</label>
+        <input type="email"
+          name="email"
+          id="email" />
       </div>
       <div>
-        <input type="radio" name="browser" value="english" onChange={({ target }) => setLenguages(target.value)} /> english
-        <input type="radio" name="browser" value="rassia" onChange={({ target }) => setLenguages(target.value)} /> rassia
-        <input type="radio" name="browser" value="kyrgyz" onChange={({ target }) => setLenguages(target.value)} /> kyrgyz
+        <label for="phone">Phone</label>
+        <input type="tel"
+          name="phone"
+          id="phone"
+          minlength="10"
+          maxlength="10"
+          required />
       </div>
       <div>
-        jender
-        <select onChange={({ target }) => setJender(target.value)}>
-          <option>man</option>
-          <option>woman</option>
+        <label for="dateOfBirth">Date of birth</label>
+        <input type="date"
+          name="dateOfBirth"
+          id="dateOfBirth"
+          required />
+      </div>
+      <div>
+        <label for="gender">Gender</label>
+        <select name="gender"
+          required
+          id="gender">
+          <option value="">- Select -</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
         </select>
       </div>
       <div>
-        <input type="text" onChange={({ target }) => setAddress(target.value)} /> address
+        <label>Education</label>
+        <div>
+          <label for="school">
+            <input type="radio"
+              name="education"
+              id="school"
+              value="school"
+              required /> School
+          </label>
+        </div>
+        <div>
+          <label for="university">
+            <input type="radio"
+              name="education"
+              id="university"
+              value="university"
+              required /> University
+          </label>
+        </div>
       </div>
       <div>
-        <input type="text" onChange={({ target }) => setCity(target.value)} /> city
+        <label>Languages</label>
+        <div>
+          <label for="english">
+            <input type="checkbox"
+              name="language"
+              id="english"
+              value="english" /> English
+          </label>
+        </div>
+        <div>
+          <label for="kyrgyz">
+            <input type="checkbox"
+              name="language"
+              id="kyrgyz"
+              value="kyrgyz" /> Kyrgyz
+          </label>
+        </div>
+        <div>
+          <label for="russian">
+            <input type="checkbox"
+              name="language"
+              id="russian"
+              value="russian" /> Russian
+          </label>
+        </div>
       </div>
       <div>
-        <input type="email" id="email" onChange={({ target }) => setEmail(target.value)} /> email
+        <label for="address">Address</label>
+        <input type="text"
+          name="address"
+          id="address"
+          required />
       </div>
       <div>
-        <input type="tel" id="phone" name="phone" onChange={({ target }) => setTel(target.value)} /> phone
+        <label for="city">City</label>
+        <input type="text"
+          name="city"
+          id="city"
+          required />
       </div>
       <div>
-        <input type="radio" name="educational" value="shcool" onChange={({ target }) => setEducation(target.value)} /> shcool
-        <input type="radio" name="educational" value="university" onChange={({ target }) => setEducation(target.value)} /> university
+        <label for="zip">ZIP</label>
+        <input type="number"
+          name="zip"
+          id="zip"
+          min="720000"
+          max="740000"
+          required />
       </div>
       <div>
-        <input type="number" onChange={({ target }) => setZip(target.value)}/> zip
+        <button>Submit</button>
       </div>
-    </div>
+    </form>
   );
 }
 
